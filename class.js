a1220e35e92e47308fb917e55ec64242e8e94c52
Jahid0178 <Jahidulhaque767@gmail.com>
2021-08-24 19:27:40 +0600

    class js upload

---
+++ class.js
@@ -0,0 +1,18 @@
+class Support {
+    name;
+    role = 'Support web developer';
+    address = 'BD';
+    constructor (name, address) {
+        this.name = name;
+        this.address = address;
+    }
+    StartSession () {
+        console.log(this.name, 'start a support session');
+    };
+};
+
+const jahid = new Support('Jahidul Haque', 'Savar');
+const rohim = new Support('Rohim Uddin', 'Dhaka');
+// console.log(jahid, rohim);
+jahid.StartSession();
+rohim.StartSession();
